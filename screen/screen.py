868df357c5b108bfb5f544620bdd5c4f7523b8a8
hhgttg <ksatoshi119@gmail.com>
2025-07-24 09:19:55 +0900

    updateで書き直す前

--- screen/screen.py
+++ screen/screen.py
@@ -32,23 +32,23 @@ def key_input(l):
                 else:
                     return(False)
 
-def key_AZ(c):
-    c = c + 32
-    result = False
-    while True:
-        for k in KEY_AZ + KEY_SRE:
-            if pyxel.btnp(k):
-                if (k in KEY_AZ) and (k <= c):
-                    return(chr(k-32)) # 返り値は chr１文字
-                elif k in KEY_SRE:
-                    return(False)
+def key_AZ():
+    for k in KEY_AZ:
+        if pyxel.btn(k):
+            return(k)
+    return(False)
 
 def key_09(): #0-9の整数が返る
     result = False
-    for k in KEY_09:
-        if pyxel.btnp(k):
-            result = k - ord('A')
-    return(result)
+    while True:
+        if pyxel.btnp(pyxel.KEY_A):
+                result = pyxel.KEY_A
+                return(result)
+        # for k in KEY_09:
+        #     print(k)
+        #     if pyxel.btnp(k):
+#                result = k
+                return(result)
 
 def key_1n(n):
     result = False
@@ -287,35 +287,13 @@ if __name__=="__main__":
     pyxel.init(SCREEN_WIDTH,SCREEN_HEIGHT)
     pyxel.cls(0)
     
-    c = 5
-    i = 0
-    for y in range(0,30):
-        text12(0,y,f"{y}",c)
-        i += 1
-        if i==10:
-            i=0
-            c += 1
-    c = 5
-    i = 0
-    for x in range(0,30):
-        text12(x,0,f"{i}",c)
-        i += 1
-        if i==10:
-            i=0
-            c += 1
-#     title_color(0,2)
-#     title_center(0,"ボルタック交易所",7)
+    a = None
+    while True:
+        print(a)
+        if pyxel.btnp(pyxel.KEY_A):
+            a = pyxel.KEY_A
+            print(f"a={a}")
+            break
+    print(a)
 
-#     l = ['あああ','いいい','かきくけこ','あめんぼ','あああ','いいい','かきくけこ','あかなょん','あああ','いいい','かきくけこ','あめんぼなさかなょん']
-#     draw_list(l,1,alphabet=True)
-#     line_horizontal(11,"- ")
-# #    line_horizontal(20,"- ")
-#     title_color(20,7)
-#     title_left(20," #  N A M E                 J O B     S T A T U S",0)
-#     l = ['あかさたなはまやらわ','いきしちにひみいりい','うくすつぬふむゆるう','えけせてねへめえれえ']
-#     draw_list(l,21,number=True)
-#     # line_horizontal(23,"- ")
-#     # line_horizontal(24,"# ")
-#     # line_horizontal(25,"あ")
-
-    pyxel.show()        
+#    pyxel.show()        
